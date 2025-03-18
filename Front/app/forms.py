238f0908a937ec0bf87f556f4c7fe5889f4cd65e
Remi Cazelles <remi.cazelles.fr@gmail.com>
2025-03-18 16:00:44 +0100

    docker compose ok --> pblm ports

---
+++ Front/app/forms.py
@@ -0,0 +1,126 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SelectField, StringField, SubmitField, DateTimeField, FloatField, IntegerField, TimeField, BooleanField, PasswordField
+from wtforms.validators import DataRequired, Length, NumberRange, NoneOf, AnyOf, ValidationError, Email
+
+import datetime
+import re
+from app import config
+########################################
+#####  Create forms to login users #####
+########################################
+
+allowed_emails = config.allowed_emails
+allowed_email_pattern = config.allowed_email_pattern
+def email_pattern_allowed(form, field):
+    if field.data not in allowed_emails:
+        raise ValidationError('Email is not in the allowed list of emails.')
+    if not re.search(allowed_email_pattern, field.data):
+        raise ValidationError('Email must end with @carbon-waters.com.')
+    
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email(),email_pattern_allowed])
+    password = PasswordField('Password', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class DeleteAccount(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+    
+########################################
+#####  Create forms to gather data #####
+########################################
+class Form_Matieres_premieres(FlaskForm):
+    MP_nom = SelectField('Type', choices=config.matieres_premieres)
+    MP_ref_fournisseur = StringField('ref fournisseur', [Length(max=50),DataRequired()]) 
+    MP_date_reception = DateTimeField('Date reception', format='%d/%m/%y')
+    MP_quantite = FloatField('Quantité', default=20)
+    MP_unite = SelectField('unite', choices=config.unitees)
+ # name of batch + date of analysis + type of analysis + details analysis
+                      # "nom_250129_Raman / nom_250202_DLS / .."
+    MP_stock = FloatField('Stock', default=20)
+    # submit = SubmitField('Submit New matiere premiere')
+
+
+class Form_analyses(FlaskForm):
+    Analyse_path_to_raw = StringField('local_path', [Length(max=10)])
+    Analyse_code = StringField('code_analyse', [Length(max=10)]) 
+    submit = SubmitField('Submit New Analyse')
+
+
+class Form_Batch_KC8(FlaskForm):
+    Batch_KC8_name = StringField('Nom Batch', validators=[NoneOf([],message='Ce batch existe déjà'),Length(max=10)])
+    Batch_KC8_date = DateTimeField('Date début production', format='%d/%m/%y')
+    Batch_KC8_Technicien = SelectField('Technicien', choices=config.Techniciens_CW)
+    Batch_KC8_K_batch =  SelectField('Batch_K_name',choices=[])
+    Batch_KC8_C_batch =  SelectField('Batch_C_name',choices=[])
+    Batch_KC8_masse = FloatField('Masse totale (Kg)', default=0.6)
+    Batch_KC8_Temperature = FloatField('Température agitation', default=50)
+    Batch_KC8_Agitation = IntegerField('Vitesse agitation',default=250)
+    Batch_KC8_heure_debut = TimeField('Heure de début', format='%H:%M')
+    Batch_KC8_heure_fin = TimeField('Heure de fin', format='%H:%M')
+    Batch_KC8_room_HR = FloatField('Humidité (ppm)', default=0.09)
+    Batch_KC8_room_T = FloatField('Temperature (°C)', default=25)
+    Batch_KC8_Stock = FloatField('Stock de KC8', default=0)
+    Batch_KC8_Analyses = StringField('reference_analyses', [Length(max=60)])
+    # def validate_unique_batch(self, field):
+
+
+class Form_Batch_OGD(FlaskForm):
+    Batch_OGD_name = StringField('Nom Batch', validators=[NoneOf([],message='Ce batch existe déjà'),Length(max=10)])
+    Batch_OGD_date = DateTimeField('Date début production', format='%d/%m/%y')
+    Batch_OGD_Technicien = SelectField('Technicien', choices=config.Techniciens_CW)
+    Batch_OGD_KC8_batch =  SelectField('Batch_KC8_name',choices=[])
+    Batch_OGD_KC8_masse = FloatField('Masse KC8 utlilisé', default=0.6)
+    Batch_OGD_THF_batch = SelectField('Batch_THF_name',choices=[])
+    Batch_OGD_THF_Volume = IntegerField('Volume (L)',default=30)
+    Batch_OGD_Temperature = FloatField('Température agitation', default=50)
+    Batch_OGD_Agitation = IntegerField('Vitesse agitation',default=250)
+    Batch_OGD_heure_debut = TimeField('Heure de début', format='%H:%M')
+    Batch_OGD_heure_fin = TimeField('Heure de fin', format='%H:%M')
+    Batch_OGD_room_HR = FloatField('Humidité (ppm)', default=0.09)
+    Batch_OGD_room_T = FloatField('Temperature (°C)', default=25)
+    Batch_OGD_Stock = FloatField('Stock de OGD', default=0)
+    Batch_OGD_Analyses = StringField('reference_analyses', [Length(max=60)])
+
+class Form_Produit(FlaskForm):
+    Batch_Produit_ref_CW = StringField('Nom Batch', [Length(max=10)])
+    Batch_Produit_date = DateTimeField('Date début production', format='%d/%m/%y')
+    Batch_Produit_Technicien = SelectField('Technicien', choices=config.Techniciens_CW)
+    Batch_Produit_OGD_batch =  SelectField('Batch_OGD_name', choices=[])
+    Batch_Produit_OGD_Qte = FloatField('Qte OGD utitisé', default=50)
+    Batch_Produit_additif_batch = SelectField('Batch_additif_name',choices=['Viscosant','Epikote1001','Epikote827'])
+    Batch_Produit_additif_Qte = FloatField('Additif Qte',default=250)
+    Batch_produit_stock = FloatField('Stock de produit (L)',default=0)
+    Batch_Produit_Analyses = StringField('reference_analyses', [Length(max=60)])
+
+class Form_Envoi(FlaskForm):
+    Envoi_date_commande = DateTimeField('Date commande', format='%d/%m/%y')
+    Envoi_client_name = SelectField('Nom Client', choices=[])
+    Envoi_produit_name = SelectField('Type', choices=config.Produits_CW)
+    Envoi_produit_batch = SelectField('Reference Batch', choices=[]) 
+    Envoi_produit_Qte = FloatField('Qte produit', default=50)
+    Envoi_produit_emballage =  StringField('Conditionnement', [Length(max=10)])
+    Envoi_date_prevu = DateTimeField('Date envoi prevu', format='%d/%m/%y')
+    Envoi_date_effective = DateTimeField('Date envoi effective', format='%d/%m/%y')
+    Envoi_code_coli = StringField('Reference coli', [Length(max=50)])
+    Envoi_delivered = DateTimeField('Date reception', format='%d/%m/%y')
+    Envoi_retour_client = TextAreaField('Metadata')
+    Envoi_submit = SubmitField('Valider')
+
+class Confirm_delete(FlaskForm):
+    submit = SubmitField('Confirm Delete')
+
+
+class Form_calculate_K_C(FlaskForm):
+    submit = SubmitField('Calculate')
+    
+class Form_Client(FlaskForm):
+    Client_name = StringField('Nom client')
+    Client_adresse = StringField('Adresse client')
---
+++ Front/tests/test_utils.py
@@ -0,0 +1,47 @@
+from ..utils import predict_OGD_concentration
+import datetime
+from unittest.mock import patch, Mock
+import pandas as pd
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def test_predict_OGD_concentration():
+    # prepare fake data
+    data = {'Batch_OGD_name': 'OGD2512', 'Batch_OGD_date': '2025-03-17T00:00:00', 'Batch_OGD_Technicien': 'FB', 'Batch_OGD_KC8_batch': 'K2450', 'Batch_OGD_KC8_masse': '0', 'Batch_OGD_THF_batch': 'fournisseur1', 'Batch_OGD_THF_Volume': '30', 'Batch_OGD_Temperature': '50', 'Batch_OGD_Agitation': '250', 'Batch_OGD_heure_debut': '2025-03-17T22:53:00', 'Batch_OGD_heure_fin': None, 'Batch_OGD_room_HR': '0.09', 'Batch_OGD_room_T': '25', 'Batch_OGD_Stock': None, 'Batch_OGD_Analyses': 'None'}
+    heure_debut =  datetime.datetime.fromisoformat(data['Batch_OGD_heure_debut'])
+    pred = predict_OGD_concentration(data,heure_debut)
+
+    # Expected response from the API
+    expected_response = {'pred': 0.508065}
+
+    # Mock the requests.post method
+    with patch('requests.post') as mock_post:
+        # Configure the mock to return a response with the expected JSON data
+        mock_response = Mock()
+        mock_response.json.return_value = expected_response
+        mock_post.return_value = mock_response
+
+        # Call the function
+        result = predict_OGD_concentration(data, heure_debut)
+
+        # Prepare the expected data for the assertion
+        expected_data = data.copy()
+        expected_data['Batch_OGD_heure_fin'] = (heure_debut + pd.DateOffset(days=6)).isoformat()
+        expected_data['Batch_OGD_Analyses'] = "1"
+        expected_data['Batch_OGD_Stock'] = 0
+
+        # Assertions
+        assert result == expected_response['pred']
+        mock_post.assert_called_once_with(
+            url="http://127.0.0.1:8003/predict/elasticnet",
+            headers={
+                'Supervized-API-Key': os.getenv('API_SUPERVIZED_SECRET_KEY'),
+                'Content-Type': 'application/json'
+            },
+            data=json.dumps(expected_data)
+        )
+        
+
